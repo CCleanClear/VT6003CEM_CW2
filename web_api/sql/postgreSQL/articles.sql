36db9365c02a4e766a2be1d576bae0d9b640b217
CrystalSchoolWork <sevenl1227@gmail.com>
2024-06-04 17:04:21 +0800

    finish the insert, update, delete dog function

--- web_api/sql/postgreSQL/articles.sql
+++ web_api/sql/postgreSQL/articles.sql
@@ -55,12 +55,12 @@ CREATE TABLE public.articles (
   
 );
 
-INSERT INTO articles (dogname, breed,summary, imageurl, location, writerid, description) VALUES
+INSERT INTO dogs (dogname, breed,summary, imageurl, locationid, writerid, description) VALUES
 	('Max', 'Labrador Retriever','Age: 3 years
-Medical Records: Up-to-date vaccinations, neutered, and microchipped','http://localhost:10888/api/v1/images/ef8ac20c-1eeb-4605-92cb-0aaf7e873777',"The Canine Shelter" ,1,'Max is a friendly and energetic Labrador Retriever. He loves playing fetch.'),
+Medical Records: Up-to-date vaccinations, neutered, and microchipped','http://localhost:10888/api/v1/images/ef8ac20c-1eeb-4605-92cb-0aaf7e873777',2 ,1,'Max is a friendly and energetic Labrador Retriever. He loves playing fetch.'),
 	('Apple', 'German Shepherd','Age: 2 years
-Medical Records: Vaccinations up to date, spayed, and heartworm negative','http://localhost:10888/api/v1/images/7e484eb6-afa7-47ec-a76a-2ed4bec73067',"The Canine Shelter", 4,'Apple is a loyal and intelligent German Shepherd.  She is house-trained and knows basic commands.'),
+Medical Records: Vaccinations up to date, spayed, and heartworm negative','http://localhost:10888/api/v1/images/7e484eb6-afa7-47ec-a76a-2ed4bec73067',3, 4,'Apple is a loyal and intelligent German Shepherd.  She is house-trained and knows basic commands.'),
 	('John', 'Golden Retriever','Age: 5 years
-Medical Records: Vaccinated, spayed, and in good health','http://localhost:10888/api/v1/images/dacf6518-110f-4046-846a-f8619093cce9', "The Canine Shelter", 1,'John is a gentle Golden Retriever.' ),
+Medical Records: Vaccinated, spayed, and in good health','http://localhost:10888/api/v1/images/dacf6518-110f-4046-846a-f8619093cce9', 2, 1,'John is a gentle Golden Retriever.' ),
 	('Rocky', 'Bulldog', 'Age: 4 years
-Medical Records: Neutered, healthy', 'http://localhost:10888/api/v1/images/bfd79cf7-806c-4f0a-bfbd-10a15e5561ff3',"The Canine Shelter", 4,'Rocky is a playful and friendly Bulldog.');
+Medical Records: Neutered, healthy', 'http://localhost:10888/api/v1/images/bfd79cf7-806c-4f0a-bfbd-10a15e5561ff3',2, 4,'Rocky is a playful and friendly Bulldog.');
